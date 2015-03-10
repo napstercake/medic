3d55b88564821a132a61c76932a0606915b48e29
Ricardo Gonzales <js.ricardo.gonzales@gmail.com>
2015-03-10 17:25:57 -0500

    Comment the code

--- Clinica/Clinica/src/com/cqr/xml/XMLParserCita.java
+++ Clinica/Clinica/src/com/cqr/xml/XMLParserCita.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This is a software created by me, If you have any question about this project
+ * just ask or make a pull request for this project.
+ * 
+ * @author Ricardo Gonzales [js.ricardo.gonzales@gmail.com]
  */
 
 package com.cqr.xml;
