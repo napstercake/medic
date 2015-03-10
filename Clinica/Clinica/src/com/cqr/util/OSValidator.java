9c015776e13f91c0ed5b332139372e1deb9faf08
Ricardo Gonzales <js.ricardo.gonzales@gmail.com>
2015-03-10 17:25:09 -0500

    Comment the code

--- Clinica/Clinica/src/com/cqr/util/OSValidator.java
+++ Clinica/Clinica/src/com/cqr/util/OSValidator.java
@@ -1,22 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This is a software created by me, If you have any question about this project
+ * just ask or make a pull request for this project.
+ * 
+ * @author Ricardo Gonzales [js.ricardo.gonzales@gmail.com]
  */
 
 package com.cqr.util;
 
 /**
  *
- * @author ricardogonzales
+ * @class This class is used to validate the OS.
  */
 public class OSValidator {
     
     private String OS = System.getProperty("os.name").toLowerCase();
     
+    /**
+     * Constructor.
+     */
     public OSValidator() {
     }
     
+    /**
+     * Get OS.
+     * 
+     * @return String
+     */
     public String getOs() {
         String os = "undefined";
         
@@ -35,18 +44,38 @@ public class OSValidator {
         return os;
     }
     
+    /**
+     * Get windows flag.
+     * 
+     * @return  boolean.
+     */
     private boolean isWindows() {
         return (OS.indexOf("win") >= 0);
     }
 
+    /**
+     * Get osx flag.
+     * 
+     * @return boolean.
+     */
     private boolean isMac() {
         return (OS.indexOf("mac") >= 0);
     }
 
+    /**
+     * Get unix flag.
+     * 
+     * @return boolean.
+     */
     private boolean isUnix() {
         return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
     }
 
+    /**
+     * Get solaris flag.
+     * 
+     * @return boolean.
+     */
     private boolean isSolaris() {
         return (OS.indexOf("sunos") >= 0);
     }
