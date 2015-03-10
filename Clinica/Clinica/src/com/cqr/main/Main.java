9a631c5fdf24b6740836250dee70c87138ae9af9
Ricardo Gonzales <js.ricardo.gonzales@gmail.com>
2015-03-10 17:24:53 -0500

    Comment the code

--- Clinica/Clinica/src/com/cqr/main/Main.java
+++ Clinica/Clinica/src/com/cqr/main/Main.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This is a software created by me, If you have any question about this project
+ * just ask or make a pull request for this project.
+ * 
+ * @author Ricardo Gonzales [js.ricardo.gonzales@gmail.com]
  */
 
 package com.cqr.main;
@@ -13,11 +14,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
- * @author ricardogonzales
+ * Main Class.
+ * 
+ * @class Main class for the whole application.
  */
 public class Main extends Application{
 
+    /**
+     * Prepare stage.
+     * 
+     * @param stage
+     * @throws Exception 
+     */
     @Override
     public void start(Stage stage) throws Exception {
         
@@ -28,12 +36,22 @@ public class Main extends Application{
         stage.show();
     }
     
+    /**
+     * Main.
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         
         System.out.println("Entrega 7 - Mac. 25/12/2014");
         System.out.println("- 1. Validar solo 3 citas al dia (check)");
-        System.out.println("- 2. Poner el numero de historia para impresion");
-        System.out.println("- 3. Cambiar a 3 columnas la hoja de impresion.");
+        System.out.println("- 2. Poner el numero de historia para impresion (check)");
+        System.out.println("- 3. Cambiar a 3 columnas la hoja de impresion. (check)");
+        
+        System.out.println("Posible enhacements - Marzo, 07-2015 [Pending]");
+        System.out.println("- 1. Add properties files");
+        System.out.println("- 2. Make jUnit Test");
+        System.out.println("- 3. Cleaning up the code to avoid redoundant methods.");
         
         launch(args);
     }
